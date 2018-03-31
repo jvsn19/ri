534a368de3641eb0c3457d3a491bc56766f671c6
rcrs3 <rcrs3@cin.ufpe.br>
2018-03-31 14:25:36 -0300

    Start collect pages for the classification

--- classifier/takePage.py
+++ classifier/takePage.py
@@ -2,9 +2,9 @@ import requests as rq
 from bs4 import BeautifulSoup
 
 headers = {'user-agent': 'Rodrigo'};
-url = 'http://store.steampowered.com/app/734580/BattleRush/';
+url = 'http://store.steampowered.com/app/601050/Attack_on_Titan_2__AOT2/';
 r = rq.get(url, headers=headers);
 page = BeautifulSoup(r.text, "html.parser");
 
-with open("steam/positivePages/page4.html", "w") as file:
+with open("steam/positivePages/page10.html", "w") as file:
     file.write(str(page));
