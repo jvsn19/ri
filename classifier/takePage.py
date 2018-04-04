d1c5751ae63c6566f2757d515f35a27d15fa6dbe
rcrs3 <rcrs3@cin.ufpe.br>
2018-04-03 21:46:07 -0300

    start classify pages

--- classifier/takePage.py
+++ classifier/takePage.py
@@ -1,10 +1,14 @@
 import requests as rq
 from bs4 import BeautifulSoup
 
-headers = {'user-agent': 'Rodrigo'};
-url = 'http://store.steampowered.com/app/601050/Attack_on_Titan_2__AOT2/';
-r = rq.get(url, headers=headers);
-page = BeautifulSoup(r.text, "html.parser");
+headers = {'user-agent': 'Rodrigo'}
+#url of the html page
+url = ''
+r = rq.get(url, headers=headers)
+page = BeautifulSoup(r.text, "html.parser")
 
-with open("steam/positivePages/page10.html", "w") as file:
-    file.write(str(page));
+#Y = 'negative' or 'positive'
+#X = number of the html page
+# foldername = name of the site  
+with open("foldername/YPages/pageX.html", "w") as file:
+    file.write(str(page))
