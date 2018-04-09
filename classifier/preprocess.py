7212e60a5a31e54f6a18af7c27cd5712af818d0c
rcrs3 <rcrs3@cin.ufpe.br>
2018-04-09 20:05:24 -0300

    Take only the text of html pages

---
+++ classifier/preprocess.py
@@ -0,0 +1,23 @@
+import nltk
+import glob
+import re
+from bs4 import BeautifulSoup
+
+def removeHtmlContent(path):
+    page = BeautifulSoup(open(path), "html.parser")
+    [s.extract() for s in page("script")]
+    #print(page.get_text().encode("utf-8"))
+    return page.get_text().encode("utf-8")
+
+def findAllHtmlFiles():
+    files = glob.glob("*/*/*.html")
+    
+    for i in range(len(files)):
+        path_output = re.sub('.html', '.txt', files[i])
+    
+        file = open(path_output, "w")
+        file.write(removeHtmlContent(files[i]))
+        file.close()
+        
+
+findAllHtmlFiles()
